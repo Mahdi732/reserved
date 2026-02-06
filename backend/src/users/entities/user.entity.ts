9b9d3f3da8043c879234f583a4ed44b3768c6341
Mahdi732 <ey449751@gmail.com>
2026-02-06 16:55:20 +0100

    add user.entity.ts to define User schema with Mongoose integration for user management

---
+++ backend/src/users/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { UserRole } from '../user-role.enum';
+
+export type UserDocument = User & Document;
+
+@Schema({ timestamps: true })
+export class User {
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ type: String, enum: UserRole, default: UserRole.PARTICIPANT })
+  role: UserRole;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
