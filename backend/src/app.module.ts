dcfe9e8b0e03355c09bc9b1680602b8a3835221e
Mahdi732 <ey449751@gmail.com>
2026-02-06 16:48:17 +0100

    add app.module.ts to set up main application module with necessary imports

---
+++ backend/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { validationSchema } from './config/env.validation';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { EventsModule } from './events/events.module';
+import { ReservationsModule } from './reservations/reservations.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, validationSchema }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+    }),
+    UsersModule,
+    AuthModule,
+    EventsModule,
+    ReservationsModule,
+  ],
+})
+export class AppModule {}
